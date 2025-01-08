c250a61016ed9e99cfa2aac04e7ddc15c6673c66
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-01-08 22:44:05 +0900

    [level 0] Title: n 번째 원소부터, Time: 4.50 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=n-1; i<num_list.length; i++){
+            list.add(num_list[i]);
+        }
+        
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        
+        return answer;
+    }
+}
