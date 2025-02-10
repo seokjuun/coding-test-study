bf04daadff4f0ce26c4a76341ce967e59cf3240c
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-02-10 12:27:52 +0900

    [level 2] Title: 올바른 괄호, Time: 13.24 ms, Memory: 55.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(int i=0; i< s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            } else if(stack.isEmpty()){
+                return false;
+            } else stack.pop();
+        }
+        
+        if (!stack.isEmpty() && stack.pop() == '(') return false;
+        return answer;
+    }
+}
