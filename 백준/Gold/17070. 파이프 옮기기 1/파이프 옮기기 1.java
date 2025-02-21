3e5dc2d8810e9b81f8c7fa1731b3ae87488e6aaf
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-02-21 14:03:18 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 204 ms, Memory: 17484 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,count;
+    static int[][] map;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        count =0;
+        dfs(0,1, 0); // x , y, 이동방법
+        System.out.println(count);
+    }
+
+    static void dfs(int y, int x, int d){
+        // 끝에 도착하면?
+        if(y==N-1 && x==N-1){
+            count ++;
+            return;
+        }
+        // 가로로 움직일때
+        if (d == 0 || d == 2) {
+            if (x + 1 < N && map[y][x + 1] == 0) {
+                dfs(y, x + 1, 0);
+            }
+        }
+        // 세로로
+        if (d == 1 || d == 2) {
+            if (y + 1 < N && map[y + 1][x] == 0) {
+                dfs(y + 1, x, 1);
+            }
+        }
+        // 대각
+        if (x + 1 < N && y + 1 < N) {
+            if (canmove(y, x)) {
+                dfs(y + 1, x + 1, 2);
+            }
+        }
+
+    }
+
+    //대각 검사
+    static  boolean canmove(int y, int x){
+        return map[y][x + 1] == 0 && map[y + 1][x] == 0 && map[y + 1][x + 1] == 0;
+    }
+}
