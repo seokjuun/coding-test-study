ffffed073b79b27a6500a1eaedce5f817dc1452e
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-03-14 21:33:23 +0900

    [Bronze I] Title: 약수, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] arr;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = arr[0] * arr[N-1];
+        System.out.println(answer);
+    }
+}
