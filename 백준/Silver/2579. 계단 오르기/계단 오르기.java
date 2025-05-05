826f0aa342dbcecabdde23f617709de12375aaf0
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-05-06 01:51:10 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,26 +1,30 @@
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] score = new int[N+1];
+        int[] dp = new int[N+1];
 
-        int[] score = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < N+1; i++) {
             score[i] = Integer.parseInt(br.readLine());
         }
 
-        int[] dp = new int[n];
-
-        if (n >= 1) dp[0] = score[0];
-        if (n >= 2) dp[1] = score[0] + score[1];
-        if (n >= 3) dp[2] = Math.max(score[0] + score[2], score[1] + score[2]);
-
-        for (int i = 3; i < n; i++) {
-            dp[i] = Math.max(dp[i - 2], dp[i - 3] + score[i - 1]) + score[i];
+        if (N == 1) {
+            System.out.println(score[1]);
+            return;
         }
 
-        System.out.println(dp[n - 1]);
+        dp[1] = score[1];
+        dp[2] = dp[1] + score[2];
+        
+        for (int i = 3; i < N+1; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3]+score[i-1]) + score[i];
+        }
+        
+        System.out.println(dp[N]);
     }
 }
