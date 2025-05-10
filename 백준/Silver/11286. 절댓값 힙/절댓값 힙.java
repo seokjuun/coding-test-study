9a5df77411a489cc2c71617aa60dcccf97e100d6
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-05-10 23:47:21 +0900

    [Silver I] Title: 절댓값 힙, Time: 276 ms, Memory: 25844 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a,b) -> {
+            int numA = Math.abs(a);
+            int numB = Math.abs(b);
+
+            if (numA == numB){
+                return a-b;
+            } else{
+                return numA-numB;
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0){
+                if (queue.isEmpty()){
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.offer(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
