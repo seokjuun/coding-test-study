6efdb8c6999be0063fbf0806f359de55e5a48e7d
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-11-21 19:27:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 60.85 ms, Memory: 66.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Deque<Character> stack = new ArrayDeque<>();
+        
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty() && stack.peek() == c){
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+            
+            
+        }
+        
+        return stack.isEmpty() ? 1:0;
+    }
+    
+    
+}
