314caa345e636a63c49412c5d3d3486b8ff2f589
seokjun Hong <45346977+seokjuun@users.noreply.github.com>
2025-12-17 15:22:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.00 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,56 +1,58 @@
 import java.util.*;
 
 class Solution {
-    static List<Integer>[] listArr;  
+    static List<Integer>[] list;
+    static int min = Integer.MAX_VALUE;
     
     public int solution(int n, int[][] wires) {
-        listArr = new ArrayList[n+1]; // 송전탑 개수만큼의 크기를 가지는 리스트 배열 (1부터)
-        
-        for(int i = 1; i<=n; i++){ // 리스트 초기화 (1부터 n까지)
-            listArr[i] = new ArrayList<>();
+        list = new ArrayList[n+1];
+        for(int i =1; i<n+1; i++){
+            list[i] = new ArrayList<>();
         }
-        // 전선 연결
+        
         for(int[] wire : wires){
-            int a = wire[0];
-            int b = wire[1];
+            int v1 = wire[0];
+            int v2 = wire[1];
             
-            listArr[a].add(b);
-            listArr[b].add(a);
+            list[v1].add(v2);
+            list[v2].add(v1);
         }
         
-        // 전선 개수는 n-1개 일거고 n-1개의 전선을 모두 달아 최소값 구한 뒤 리턴?
-        int min = 100;
-        for(int[] wire : wires){
-            int num = check(wire[0], wire[1], listArr, n);
-            min = Math.min(min, num);
+        for(int[] wire: wires){
+            // 하나씩 끊어보기
+            int v1 = wire[0];
+            int v2 = wire[1];
+            
+            min = Math.min(countNode(v1,v2,n), min);
         }
         
         return min;
     }
-    // 한쪽 정점에 대해서만 다른쪽 정점 나오면 거르고 개수 센다면 한번만 확인 하면됨 
-    static int check(int a, int b, List<Integer>[] listArr, int n){
+    
+    static int countNode(int v1, int v2, int n){
+        int count = 1;
         boolean[] visited = new boolean[n+1];
         
         Deque<Integer> queue = new ArrayDeque<>();
-        queue.offer(a);
-        visited[a] = true;
-        int cnt = 1;
+        queue.add(1);
+        visited[1] = true;
         
         while(!queue.isEmpty()){
             int poll = queue.poll();
             
-            for(Integer num : listArr[poll]){
-                if(num == b) continue;
-                if(!visited[num]){
-                    queue.offer(num);
-                    visited[num] = true;
-                    cnt++;
+            for(int i=0; i<list[poll].size(); i++){
+                int get = list[poll].get(i); 
+                if((poll == v1 && get == v2) || (poll == v2 && get ==v1)) continue;
+                
+                if(!visited[get]){
+                    queue.add(get);
+                    visited[get] = true;
+                    count++;
                 }
             }
         }
         
-        return Math.abs(2*cnt - n);
+        
+        return Math.abs(2*count -n);
     }
-    
-    
 }
